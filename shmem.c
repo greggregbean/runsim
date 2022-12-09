3edcacf59669c3a078a058f8f589d6cf82d02bb1
greggregbean <90098955+greggregbean@users.noreply.github.com>
2022-12-09 22:47:45 +0300

    два варианта

--- shmem.c
+++ shmem.c
@@ -28,13 +28,9 @@ void run_execl(const char* command) {
 
     if(process_id < 0) printf("Failed to create child -> child procces! \n");
 
-    else if(process_id > 0) {
-        //printf("Child -> parent proccess. PID = %d. Just go back to child. \n", getpid()); 
-    }
+    else if(process_id > 0) {}
 
     else {
-        //printf("Child -> child proccess. PID = %d. \n", getpid());
-
         int res_of_run = execl("/bin/bash", "/bin/bash", "-c", command, NULL);
         if(res_of_run == -1) {
             printf("Failed to run %s! \n", command);
@@ -48,31 +44,9 @@ void run_command (const char* command, int* cmds_in_run) {
 
     if(process_id < 0) printf("Failed to create child procces! \n");
 
-    else if(process_id > 0) {
-        //printf("Parent proccess. PID = %d. Just go back to the loop. \n", getpid()); 
-    }
+    else if(process_id > 0) {}
 
     else {
-        //printf("Child proccess. PID = %d. \n", getpid());
-
-        key_t key = 0; 
-        if ((key = ftok("main.c", 0)) < 0) {
-            printf("Can't generate a key\n");
-            exit(-1);
-        }
-
-        int shmid;
-        if ((shmid = shmget(key, sizeof(int), 0)) < 0) {
-            printf("Can't find shared memory. \n");
-            exit(-1);
-        }
-
-        int* cmds_in_run;
-        if((cmds_in_run =(int*)shmat(shmid, NULL, 0)) == (int*)(-1)) {
-            printf("Can't attach shared memory. \n");
-            exit(-1);
-        }
-
         cmds_in_run[0]++;
         printf("Cmds_in_run = %d. \n", cmds_in_run[0]); 
 
@@ -81,17 +55,11 @@ void run_command (const char* command, int* cmds_in_run) {
         cmds_in_run[0]--;
         printf("Cmds_in_run = %d. \n", cmds_in_run[0]);
 
-        if (shmdt(cmds_in_run) < 0) {
-            printf("Can't detach shared memory\n");
-            exit(-1);
-        }
-
         exit(0);
     }
 }
 
 int main (int argc, char* argv []) {
-
     int N = str_to_num(argv[1]);
     printf("N = %d. \n", N);
  
@@ -120,7 +88,7 @@ int main (int argc, char* argv []) {
         gets(command);
         fflush(stdin);
 
-        if(strcmp(command, "$") == 0) break;
+        if(strcmp(command, "^D") == 0) break;
 
         else if(cmds_in_run[0] == N) {
             printf("Too many processes are being run! Try later. \n");
---
+++ signal.c
@@ -0,0 +1,115 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <signal.h>
+
+#define COMMAND_SIZE 1000
+
+int cmds_in_run = 0;
+
+int str_to_num(char* str) {
+    int digit = 1;
+    int num = 0;
+    for(int i = (strlen(str) - 1); i != -1; i--) {
+        num += (str[i] - '0')*digit;
+        digit *= 10;
+    }
+    return num;
+}
+
+void my_handler(int sig)
+{
+    if (sig == SIGCHLD) {
+        cmds_in_run--;
+    }
+}
+
+void run_execl(const char* command) {
+    pid_t process_id = fork();
+
+    int status;
+    wait(&status);
+
+    if(process_id < 0) printf("Failed to create child -> child procces! \n");
+
+    else if(process_id > 0) {}
+
+    else {
+        int res_of_run = execl("/bin/bash", "/bin/bash", "-c", command, NULL);
+        if(res_of_run == -1) {
+            printf("Failed to run %s! \n", command);
+            exit(-1);
+        }
+    }
+}
+
+void run_command (const char* command) {
+    pid_t process_id = fork();
+
+    if(process_id < 0) printf("Failed to create child procces! \n");
+
+    else if(process_id > 0) {}
+
+    else { 
+        run_execl(command);
+        printf("Commands in run = %d. \n", cmds_in_run);
+        exit(0);
+    }
+}
+
+int main (int argc, char* argv []) {
+    if(argc == 0) {
+        printf("No args in command line. \n");
+        exit(-1);
+    }
+    int N = str_to_num(argv[1]);
+    printf("N = %d. \n", N);
+
+    signal(SIGCHLD, my_handler);
+ 
+    char command [COMMAND_SIZE] = {'\0'};
+
+    while(1) {
+        gets(command);
+        fflush(stdin);
+
+        if(strcmp(command, "$") == 0) break;
+
+        else if(cmds_in_run == N) {
+            printf("Too many processes are being run! Try later. \n");
+            continue;
+        } 
+        else {
+            cmds_in_run++;
+            printf("Commands in run = %d. \n", cmds_in_run);
+            run_command(command);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
