d5a5c173835d0fde8cde4086b0a1a761b759bbd0
greggregbean <90098955+greggregbean@users.noreply.github.com>
2022-12-09 14:03:47 +0300

    /

--- main.c
+++ main.c
@@ -159,39 +159,3 @@ int main (int argc, char* argv []) {
 
 
 
-
-// //Получение доступа к семафору
-//         key_t key;
-//         int semid;
-//         if ((key = ftok(pathname, 0)) < 0) {
-//             printf("Can't generate a key\n");
-//             exit(-1);
-//         }
-//         if ((semid = semget(key, 1, 0)) < 0) {
-//             printf("Can't get semid\n");
-//             exit(-1);
-//         }
-        
-
-
-// //Создаём семафор 
-//     key_t key;
-//     int semid;
-//     if ((key = ftok("main.c", 0)) < 0) {
-//         printf("Can't generate a key\n");
-//         exit(-1);
-//     }
-//     if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
-//         printf("Can't get semid\n");
-//         exit(-1);
-//     }
-
-//     //Создаём N свободных мест
-//     struct sembuf mybuf;
-//     mybuf.sem_op = N;
-//     mybuf.sem_flg = 0;
-//     mybuf.sem_num = 0;
-//     if (semop(semid, &mybuf, 1) < 0) {
-//         printf("Can't do the operation\n");
-//         exit(-1);
-//     }
